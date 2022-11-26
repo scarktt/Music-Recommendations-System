86d23ce633751dd951391b8a9407fc08a4e546bf
ssoteloserrano <ssoteloserrano@gmail.com>
2022-11-25 23:08:55 -0600

    Add extraction data process

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from cfg import CLIENT_ID, CLIENT_SECRET, SPOTIPY_REDIRECT_URI, USER
-
+import argparse
 import json
 import math
-import requests
 
 import pandas as pd
+import requests
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
 
-scope = "playlist-read-private"
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
+from cfg_prod import CLIENT_ID, CLIENT_SECRET, SPOTIPY_REDIRECT_URI
+
+scope = "user-library-read,playlist-read-private"
+sp = spotipy.Spotify(auth_manager=spotipy.oauth2.SpotifyOAuth(client_id=CLIENT_ID,
                                                 client_secret=CLIENT_SECRET,
                                                 redirect_uri=SPOTIPY_REDIRECT_URI,
                                                 scope=scope))
@@ -39,6 +39,15 @@ def get_saved_tracks(result={}, offset=0, saved_tracks=[]):
     return get_saved_tracks(result, offset+50, saved_tracks)
 
 
+def get_tracks_features(id_tracks):
+    limit=100
+    equally_chunks = [id_tracks[i:i+limit] for i in range(0, len(id_tracks), limit)]
+    track_features = []
+    for ids in equally_chunks:
+        track_features += sp.audio_features(tracks=ids)
+    return track_features
+
+
 def map_playlists_to_tabular(playlists: list):
     ids, names, total_tracks = [], [], []
 
@@ -54,7 +63,7 @@ def map_playlists_to_tabular(playlists: list):
     }
 
     playlists_df = pd.DataFrame(user_playlists)
-    print(playlists_df)
+    # print(playlists_df)
     return playlists_df
 
 
@@ -76,34 +85,58 @@ def map_tracks_to_tabular(tracks: list):
 
     tracks_df = pd.DataFrame(user_tracks)
     tracks_df.drop_duplicates(subset=['name', 'id', 'artist'], inplace=True)
-    print(tracks_df)
+    # print(tracks_df)
     return tracks_df
 
 
-def main():
+def extract(user:str, target_playlist:str):
     playlists_results = get_all_user_playlists()
 
-    user_own_playlists = list(filter(lambda playlist: playlist['owner']['id'] == USER, playlists_results))
+    user_own_playlists = list(filter(lambda playlist: playlist['owner']['id'] == user, playlists_results))
 
     playlist_df = map_playlists_to_tabular(user_own_playlists)
 
-    user_tracks = []
+    test_playlist = playlist_df.loc[playlist_df['name'] == target_playlist]
+
+    user_tracks, target_tracks = [], []
     for playlist in user_own_playlists:
         print(f'getting songs from {playlist["name"]} playlist...')
         playlist_id = playlist['id']
-        user_tracks += get_playlist_tracks(playlist_id)
+        if playlist["name"] == target_playlist:
+            target_tracks = get_playlist_tracks(playlist_id, {}, 0, [])
+        else:
+            user_tracks += get_playlist_tracks(playlist_id, {}, 0, [])
 
     print(f'getting saved songs...')
     user_tracks += get_saved_tracks()
 
     user_tracks_df = map_tracks_to_tabular(user_tracks)
+    target_tracks_df = map_tracks_to_tabular(target_tracks)
 
     id_tracks = user_tracks_df['id'].tolist()
-    equally_chunks = [id_tracks[i:i+n] for i in range(0, len(id_tracks), 100)]
-    audio_features = [sp.audio_features(tracks=ids) for ids in equally_chunks]
-    print(audio_features)
+    id_target_tracks = target_tracks_df['id'].tolist()
+    tracks_features = get_tracks_features(id_tracks)
+    target_tracks_features = get_tracks_features(id_target_tracks)
 
+    tracks_features_df = pd.DataFrame(tracks_features, columns = ["danceability", "energy", "key", "loudness", "mode", "speechiness", "acousticness", "instrumentalness", "liveness", "valence", "tempo", "type", "id", "uri", "track_href", "analysis_url","duration_ms", "time_signature"])
+    target_tracks_features_df = pd.DataFrame(target_tracks_features, columns = ["danceability", "energy", "key", "loudness", "mode", "speechiness", "acousticness", "instrumentalness", "liveness", "valence", "tempo", "type", "id", "uri", "track_href", "analysis_url","duration_ms", "time_signature"])
+
+    df = user_tracks_df.merge(tracks_features_df, on=['id'])
+    target_df = target_tracks_df.merge(target_tracks_features_df, on=['id'])
+    df.drop(['analysis_url', 'type', 'track_href', 'uri'], axis=1, inplace=True)
+    target_df.drop(['analysis_url', 'type', 'track_href', 'uri'], axis=1, inplace=True)
+    return df, target_df
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Generates music recommendations based on 100% own saved music.')
+    parser.add_argument('-u', '--user', required=True, help='Spotify username.')
+    parser.add_argument('-p', '--playlist', required=True, help='Spotify playlist name.')
+    parser.add_argument('--dev', default=False, action='store_true', help='Uses a dev configuration to connect with spotify (default: False)')
+    args = parser.parse_args()
+    dev = args.dev
+    user = args.user
+    playlist = args.playlist
+
+    df, target_df = extract(user, playlist)
+    print(f"{df}\n{target_df}")
 
